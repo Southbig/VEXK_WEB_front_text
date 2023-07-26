1b2a7deb88f75cb5c1ed1d9ab5c64a0649cd75d0
Southbig <tesnam1216@gmail.com>
2023-07-27 00:23:02 +0900

    eslint err 핸들링

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-shadow */
 /* eslint-disable react/button-has-type */
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 import './App.css'
 
