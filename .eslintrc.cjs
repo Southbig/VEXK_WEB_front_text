7e734d93d7c9370be74e3b495bea33a3d6136f7a
Southbig <tesnam1216@gmail.com>
2023-07-27 00:31:24 +0900

    eslint err 핸들링

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -44,7 +44,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': 'off',
   },
-  ignorePatterns: ['.eslintrc.cjs'],
 }
 
 // extends: [ 'plugin:prettier/recommended']
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
 })
+function tsconfigPaths(): import("vite").PluginOption {
+  throw new Error('Function not implemented.')
+}
+
