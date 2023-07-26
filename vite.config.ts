e4827a3f10441b1d4bfb41905e8ef9c9e36fbb79
Southbig <tesnam1216@gmail.com>
2023-07-27 00:38:40 +0900

    eslint err 핸들링

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
 })
-function tsconfigPaths(): import("vite").PluginOption {
-  throw new Error('Function not implemented.')
-}
-
