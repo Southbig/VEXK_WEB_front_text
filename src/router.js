e8817179b9f72c595a51a6d7d5b3cc5a000fb6a1
Southbig <83868259+Southbig@users.noreply.github.com>
2023-07-26 22:20:12 +0900

    Initial commit

--- src/router.js
+++ src/router.js
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import Login from './pages/login'
 
 export default router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
   },
+  {
+    path: '/login',
+    element: <Login />,
+  },
 ])
-
-// export default router
