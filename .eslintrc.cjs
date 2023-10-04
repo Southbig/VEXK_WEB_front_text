3f13fa9e8ce442a6fa55a2ed3301bc01bf6d6e12
Southbig <tesnam1216@gmail.com>
2023-10-04 11:31:21 +0900

    초기 세팅

---
+++ .eslintrc.cjs
@@ -0,0 +1,53 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    // "extends": [
+    //     "eslint:recommended",
+    //     "plugin:@typescript-eslint/recommended",
+    //     "plugin:react/recommended"
+    // ],
+    // "extends": [
+    //     "airbnb",
+    //     "airbnb/hooks",
+    //     "plugin:react/recommended",
+    //     "plugin:@typescript-eslint/recommended",
+    // ],
+    "extends": [
+        "airbnb",
+        "airbnb-typescript",
+        "airbnb/hooks",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        'plugin:prettier/recommended'
+    ],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "react",
+        'prettier'
+    ],
+    "rules": {
+    }
+}
+
+// extends: [ 'plugin:prettier/recommended']
+// plugins: ['react', '@typescript-eslint', 'prettier'],
