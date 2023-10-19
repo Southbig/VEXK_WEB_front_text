42002084910ae2bafb15543bbd61f12738326408
Southbig <tesnam1216@gmail.com>
2023-10-19 17:51:49 +0900

    Nupdate

---
+++ src/router.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import App from './App'
+
+export default router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+])
+
+// export default router
