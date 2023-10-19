da32f94602cfc97d662d147ac1ccef8b2028a547
Southbig <tesnam1216@gmail.com>
2023-10-19 17:58:54 +0900

    update

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
+import { RouterProvider } from 'react-router-dom'
 
 console.log('router', router)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
+    <RouterProvider router={router} />
     <App />
   </React.StrictMode>
 )
